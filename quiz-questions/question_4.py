79d9172e7a2c467171bebafd6583eca0898c777a
MiHHuynh <MiHHuynh@users.noreply.github.com>
2017-03-28 16:37:35 -0700

    Update question_4.py

--- quiz-questions/question_4.py
+++ quiz-questions/question_4.py
@@ -13,6 +13,9 @@ def question_4():
     What's the final value of `result`?
 
     result = a_list[3**2 - 8] + a_list[-1] + a_tuple[2]
+    
+    result = a_list[1] + a_list[-1] + a_tuple[2]
+    result = 1 + 10 + 12 = 23
     """
     # Return the correct value.
-    pass
+    return 23
